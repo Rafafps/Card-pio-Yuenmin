60d89ba89cca6386caba6d9623c373d510af0bfa
Raíssa Tayná <raissa.xavier@ufv.br>
2024-12-23 14:35:43 -0300

    feat: adiciona SplashScreen

---
+++ app/src/main/java/br/ufv/tp1_poo/view/SplashScreen.java
@@ -0,0 +1,23 @@
+package br.ufv.tp1_poo.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.ufv.tp1_poo.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
