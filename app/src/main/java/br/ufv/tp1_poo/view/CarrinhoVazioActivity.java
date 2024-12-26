dcf79bb09c19bf55fa9df77e83c4fa45b21cbfc1
Raíssa Tayná <raissa.xavier@ufv.br>
2024-12-26 16:01:05 -0300

    fix #4: atualiza activity

--- app/src/main/java/br/ufv/tp1_poo/view/CarrinhoVazioActivity.java
+++ app/src/main/java/br/ufv/tp1_poo/view/CarrinhoVazioActivity.java
@@ -3,7 +3,6 @@ package br.ufv.tp1_poo.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import br.ufv.tp1_poo.R;
 
@@ -15,7 +14,6 @@ public class CarrinhoVazioActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this); // Habilita o uso de tela cheia
         setContentView(R.layout.activity_carrinho_vazio);
 
         botaoVoltar = findViewById(R.id.botaoVoltar);
