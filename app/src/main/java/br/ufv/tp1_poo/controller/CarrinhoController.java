0ab2204b80421a4206069854b008d00dde1c9d03
pcRafa <rafaella.pinheiro@ufv.br>
2024-12-27 12:31:10 -0300

    fix: Alterações

--- app/src/main/java/br/ufv/tp1_poo/controller/CarrinhoController.java
+++ app/src/main/java/br/ufv/tp1_poo/controller/CarrinhoController.java
@@ -1,45 +1,21 @@
-package br.ufv.tp1_poo.controller;
-
-import br.ufv.tp1_poo.model.Produto;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Iterator;
-// implementando com produto ao inves de carrinho,
-// pela logica de não delegar ao carrinho, a aplicação das funções de carrinhoController
-// Assim, não chamando carrinho Carrinho, como anets  foi colocado, mas chamando Produtos
+package br.ufv.tp1_poo.model;
 public class CarrinhoController {
-    private List<Produto> produtos;
-    public CarrinhoController() {this.produtos = new ArrayList<>();}
-    public void adicionarProdutoCarrinho(Produto produto) {
-        for (Produto p : produtos) {
-            if (p.getNome().equals(produto.getNome()) && p.getTamanho().equals(produto.getTamanho())) {
-                p.setQuantidade(p.getQuantidade() + produto.getQuantidade());
-                return;
-            }
-        }
-        produtos.add(produto);
+    private Carrinho carrinho; // Atributo do tipo Carrinho
+    public CarrinhoController() {
+        this.carrinho = new Carrinho();
     }
-    // Remover produto do carrinho, um unico
-    public void removerProdutoDoCarrinho(String nome, String tamanho) {
-        for (Iterator<Produto> iterator = produtos.iterator(); iterator.hasNext(); ) {
-            Produto produto = iterator.next();
-            if (produto.getNome().equals(nome) && produto.getTamanho().equals(tamanho)) {
-                iterator.remove();
-                break;
-            }
-        }
+    public void adicionarProdutoAoCarrinho(Produto produto) {
+        carrinho.adicionarProduto(produto);
     }
-    // Limpar carrinho, por completo, todos os itens
-    public void limparCarrinho() {produtos.clear();}
-    public double calcularTotal() {
-        double total = 0;
-        for (Produto produto : produtos) {
-            total += produto.getPreco() * produto.getQuantidade();
-        }
-        return total;
+    public void removerProdutoDoCarrinho(Produto produto) {
+        carrinho.removerProduto(produto);
     }
-    // Obter lista de produtos no carrinho
-    public List<Produto> getProdutos() {
-        return produtos;
+
+    public void limparCarrinho() {
+        carrinho.limpar();
+    }
+    public void calculaTotal() {
+        double total = carrinho.calcularTotal();
+        System.out.println("Total do carrinho: R$ " + total);
     }
 }
